86c452bf4ea0f6aa86b67a815d8c66b24ab077ec
PabloD <derivadas@protonmail.com>
2021-04-07 13:09:28 +0200

    Arreglada estructura proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+import org.postgresql.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Connection test;
+        System.out.println("TEST");
+        for(String s : args){
+            System.out.println(s);
+        }
+    }
+}
